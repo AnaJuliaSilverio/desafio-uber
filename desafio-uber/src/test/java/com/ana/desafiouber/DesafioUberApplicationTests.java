8cc7b6110afecec97551716a5601a824faf68eef
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-10-03 12:53:11 -0300

    Initial commit

---
+++ desafio-uber/src/test/java/com/ana/desafiouber/DesafioUberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ana.desafiouber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
